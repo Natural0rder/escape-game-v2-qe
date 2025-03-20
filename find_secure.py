ea3e71b2d0bd7bad45fb5c3cf613932a65d17247
Nicolas Benhamou <102281652+Natural0rder@users.noreply.github.com>
2025-03-20 10:11:26 +0100

    Add files via upload

---
+++ find_secure.py
@@ -0,0 +1,94 @@
+from pymongo import MongoClient
+from pymongo.encryption_options import AutoEncryptionOpts
+from pymongo.encryption import ClientEncryption
+import base64
+import os
+from bson.codec_options import CodecOptions
+from bson.binary import STANDARD, UUID
+import pprint
+from your_credentials import get_credentials
+
+credentials = get_credentials()
+
+# start-key-vault
+key_vault_db = "encryption"
+key_vault_coll = "__keyVault"
+key_vault_namespace = "encryption.__keyVault"
+# end-key-vault
+
+# start-kmsproviders
+provider = "local"
+path = "./master-key.txt"
+# WARNING: Do not use a local key file in a production application
+with open(path, "rb") as f:
+    local_master_key = f.read()
+kms_providers = {
+    "local": {
+        "key": local_master_key  # local_master_key variable from the previous step
+    },
+}
+# end-kmsproviders
+
+# start-schema
+connection_string = credentials["MONGODB_URI"]
+
+unencryptedClient = MongoClient(connection_string)
+keyVaultClient = unencryptedClient[key_vault_db][key_vault_coll]
+data_key_id_1 = keyVaultClient.find_one({"keyAltNames": "dataKey1"})["_id"]
+data_key_id_2 = keyVaultClient.find_one({"keyAltNames": "dataKey2"})["_id"]
+data_key_id_3 = keyVaultClient.find_one({"keyAltNames": "dataKey3"})["_id"]
+data_key_id_4 = keyVaultClient.find_one({"keyAltNames": "dataKey4"})["_id"]
+
+encrypted_db_name = "medicalRecords"
+encrypted_coll_name = "patients"
+
+encrypted_fields_map = {
+    f"{encrypted_db_name}.{encrypted_coll_name}": {
+        "fields": [
+            {
+                "keyId": data_key_id_1,
+                "path": "patientId",
+                "bsonType": "int",
+                "queries": {"queryType": "equality"},
+            },
+            {
+                "keyId": data_key_id_2,
+                "path": "medications",
+                "bsonType": "array",
+            },
+            {
+                "keyId": data_key_id_3,
+                "path": "patientRecord.ssn",
+                "bsonType": "string",
+                "queries": {"queryType": "equality"},
+            },
+            {
+                "keyId": data_key_id_4,
+                "path": "patientRecord.billing",
+                "bsonType": "object",
+            },
+        ],
+    },
+}
+# end-schema
+
+
+# start-extra-options
+auto_encryption = AutoEncryptionOpts(
+    kms_providers,
+    key_vault_namespace,
+    encrypted_fields_map=encrypted_fields_map,
+    crypt_shared_lib_path=credentials["SHARED_LIB_PATH"],
+)
+# end-extra-options
+
+# start-client
+secure_client = MongoClient(connection_string, auto_encryption_opts=auto_encryption)
+# end-client
+
+encrypted_coll = secure_client[encrypted_db_name][encrypted_coll_name]
+
+# start-find
+print("Finding a document with encrypted client, searching on an encrypted field")
+pprint.pprint(encrypted_coll.find_one({"patientRecord.ssn": "987-65-4320"}))
+# end-find
